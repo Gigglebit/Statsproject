b1e82bfae850ef33cb9c96242786957271766dc4
Gigglebit <coolgoggle@gmail.com>
2014-07-03 08:09:56 -0700

    files were deleted

--- src/run_agent.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-
-if __name__ == "__main__":
-	p = subprocess.Popen(["sudo", "python" ,"agent_backup2.py"], stdin = subprocess.PIPE, stdout = subprocess.PIPE ,stderr = subprocess.PIPE, shell =False)
-	print p.stdout.read()
-	print p.stderr.read()
-	#p.communicate()
